3c6c34cb4efafcc87aca38edfb24cee1233e6568
José F. Romaniello <jfromaniello@gmail.com>
2013-03-14 15:47:34 -0300

    add cookie sessions to enable SSO

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 var http     = require('http');
 var express  = require('express');
 var passport = require('passport');
-var WsFederationEndpoints = require('./WsFederationEndpoints');
 
-var app = express();
 
 require('./setupPassport');
+
+var cookieSessions = require('cookie-sessions');
+var app = express();
+
 
 //configure the webserver
 app.configure(function(){
   this.set('view engine', 'ejs');
   this.set('views', __dirname + '/views');
+
   this.use(express.static(__dirname + '/public'));
+  
+  this.use(express.cookieParser());
   this.use(express.bodyParser());
+  this.use(cookieSessions({
+    session_key:    'sqlfs',
+    secret:         process.env.SESSION_SECRET
+  }));
+
   this.use(passport.initialize());
   this.use(this.router);
 });
 
-app.get('/wsfed/', 
-  function (req, res) {
-    return res.render('login', {
-      title: process.env.SITE_NAME
-    });
-  });
 
-WsFederationEndpoints.install(app);
+require('./endpoints').install(app);
 
 http.createServer(app)
     .listen(process.env.PORT || 5000);
