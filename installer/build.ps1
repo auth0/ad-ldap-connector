47c34e916802f8b3fd1c86f6a2503887f46eb15c
José F. Romaniello <jfromaniello@gmail.com>
2013-11-28 14:58:00 -0200

    add LICENSE customize installer images

--- installer/build.ps1
+++ installer/build.ps1
@@ -1,6 +1,13 @@
-#Recreate output dir
-Remove-Item adldap.msi
-Remove-Item -Recurse -Force output
+#Clean
+@(
+    'output'
+    'adldap.msi'
+    'directory.wxs'
+) |
+Where-Object { Test-Path $_ } |
+ForEach-Object { Remove-Item $_ -Recurse -Force -ErrorAction Stop }
+
+#create output dir
 mkdir output
 
 #Create a tmpdir
@@ -14,7 +21,6 @@ robocopy ..\ $tmp_dir /COPYALL /S /NFL /NDL /NS /NC /NJH /NJS /XD .git installer
 #Generate the installer
 $wix_dir="c:\Program Files (x86)\WiX Toolset v3.8\bin"
 
-Remove-Item directory.wxs
 . "$wix_dir\heat.exe" dir $tmp_dir -srd -dr INSTALLDIR -cg MainComponentGroup -out directory.wxs -ke -sfrag -gg -var var.SourceDir -sreg -scom
 . "$wix_dir\candle.exe" -dSourceDir="$tmp_dir" installer.wxs WixUI.wxs DirDialog.wxs directory.wxs -o output\
 . "$wix_dir\light.exe" -o output\adldap.msi output\installer.wixobj output\directory.wixobj output\DirDialog.wixobj output\WixUI.wixobj -cultures:en-US -ext WixUIExtension.dll
