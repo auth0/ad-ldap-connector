bfbdb96289dc9408a73a109b134dabe9fd14d9ea
José F. Romaniello <jfromaniello@gmail.com>
2013-12-09 16:57:41 -0200

    add upgrade mechanism for msi

--- installer/build.ps1
+++ installer/build.ps1
@@ -36,7 +36,7 @@ $version = $version.Trim()
 $wix_dir="c:\Program Files (x86)\WiX Toolset v3.8\bin"
 
 . "$wix_dir\heat.exe" dir $tmp_dir -srd -dr INSTALLDIR -cg MainComponentGroup -out directory.wxs -ke -sfrag -gg -var var.SourceDir -sreg -scom 
-. "$wix_dir\candle.exe" -dSourceDir="$tmp_dir" -dVers="$version" *.wxs -o output\ -ext WiXUtilExtension
+. "$wix_dir\candle.exe" -dSourceDir="$tmp_dir" -dProductVersion="$version" -dRTMProductVersion="0.0.0" -dUpgradeCode="{1072AB9E-1842-4AFA-9CF2-545462CD60E2}" *.wxs -o output\ -ext WiXUtilExtension
 . "$wix_dir\light.exe" -o output\adldap.msi output\*.wixobj -cultures:en-US -ext WixUIExtension.dll -ext WiXUtilExtension
 . "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\signtool.exe" sign /n "Auth10" .\output\adldap.msi
 
