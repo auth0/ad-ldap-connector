24487b93696c6ec01e981ae22e3cbcc340759741
José F. Romaniello <jfromaniello@gmail.com>
2014-10-10 09:12:02 -0300

    properly format log events

--- eventlog.js
+++ eventlog.js
@@ -6,10 +6,10 @@ var winston = require("winston");
 
 winston.setLevels(winston.config.syslog.levels);
 winston.remove(winston.transports.Console);
-winston.add(winston.transports.File, { 
-  maxsize: 40000, 
+winston.add(winston.transports.File, {
+  maxsize: 40000,
   maxFiles: 10,
-  level: "debug", 
+  level: "debug",
   filename: __dirname + '/logs.log',
   json: false,
   handleExceptions: true
@@ -19,19 +19,17 @@ winston.add(winston.transports.File, {
 var old_log = console.log;
 var old_error = console.error;
 
+var util = require('util');
+
 console.log = function () {
-  var message = Array.prototype.slice.call(arguments)
-                     .join(' ')
-                     .stripColors; //remove colors
+  var message = util.format.apply(util, arguments).stripColors;
   if (!message) return;
   winston.debug(message);
   old_log.apply(console, arguments);
 };
 
 console.error = function () {
-  var message = Array.prototype.slice.call(arguments)
-                     .join(' ')
-                     .stripColors; //remove colors
+  var message = util.format.apply(util, arguments).stripColors;
   if (!message) return;
   winston.error(message);
   old_error.apply(console, arguments);
