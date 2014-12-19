5c12e7a25a3ee6e75ec7b719873479864bd9152a
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2014-12-19 15:49:24 -0300

    ensure certs folder

--- connector-setup/steps/certificate.js
+++ connector-setup/steps/certificate.js
@@ -1,6 +1,6 @@
-var selfsigned = require('selfsigned');
-var fs = require('fs');
-var path = require('path');
+var selfsigned  = require('selfsigned');
+var fs          = require('fs');
+var path        = require('path');
 
 var fileNames = {
   pem: path.join(process.cwd(), 'certs', 'cert.pem'),
@@ -10,8 +10,14 @@ var fileNames = {
 module.exports = function (workingPath, info, cb) {
   if (fs.existsSync(fileNames.key)) return cb();
 
-
+  // ensure certs folder
+  var certs_folder = path.dirname(fileNames.key);
+  if (!fs.existsSync(certs_folder)) {
+    fs.mkdirSync(certs_folder);
+  }
+  
   console.log('Generating a self-signed certificate.'.yellow);
+
   var selfsigned = require('selfsigned');
   var pems = selfsigned.generate({ subj: '/CN=' + info.connectionDomain , days: 365 });
 
