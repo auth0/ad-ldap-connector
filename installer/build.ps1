a15a7441238319bfabdc01678f7ffd7ccb169d36
José F. Romaniello <jfromaniello@gmail.com>
2015-01-07 11:32:28 -0300

    allow building the installer from any path

--- installer/build.ps1
+++ installer/build.ps1
@@ -1,4 +1,11 @@
 $ErrorActionPreference = "Stop"
+# $PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+
+$ProjectPath = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\") -replace "\\$"
+$ProjectPathUnix = $ProjectPath.replace("\", "/")
+$InstallerPath = "$ProjectPath\Installer"
+
+echo "installer path is $InstallerPath"
 
 # remember to manually remove this after npm install... it fails because the path is too long.
 # Remove-Item -Recurse -Force "..\node_modules\jsonwebtoken\node_modules\jws\node_modules\base64url\node_modules\tap"
@@ -21,7 +28,8 @@ Remove-Item $tmp_dir
 mkdir $tmp_dir
 
 #Copy excluding .git and installer
-robocopy ..\ $tmp_dir /COPYALL /S /NFL /NDL /NS /NC /NJH /NJS /XD .git installer
+robocopy $ProjectPath\ $tmp_dir /COPYALL /S /NFL /NDL /NS /NC /NJH /NJS /XD .git installer
+
 Copy-Item "C:\Program Files (x86)\nodejs\node.exe" $tmp_dir\bin
 
 If (Test-Path $tmp_dir\config.json){
@@ -31,17 +39,17 @@ If (Test-Path $tmp_dir\logs.log){
     Remove-Item $tmp_dir\logs.log
 }
 
-$version = (. "node" -e "console.log(require('../package.json').version);") | Out-String
+$version = (. "node" -e "console.log(require('$ProjectPathUnix/package.json').version);") | Out-String
 
 $version = $version.Trim()
 
 #Generate the installer
 $wix_dir="c:\Program Files (x86)\WiX Toolset v3.8\bin"
 
-. "$wix_dir\heat.exe" dir $tmp_dir -srd -dr INSTALLDIR -cg MainComponentGroup -out directory.wxs -ke -sfrag -gg -var var.SourceDir -sreg -scom
-. "$wix_dir\candle.exe" -dSourceDir="$tmp_dir" -dProductVersion="$version" -dRTMProductVersion="0.0.0" -dUpgradeCode="{1072AB9E-1842-4AFA-9CF2-545462CD60E2}" *.wxs -o output\ -ext WiXUtilExtension
-. "$wix_dir\light.exe" -o output\adldap.msi output\*.wixobj -cultures:en-US -ext WixUIExtension.dll -ext WiXUtilExtension -ext WiXNetFxExtension
-. "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\signtool.exe" sign /n "Auth0" .\output\adldap.msi
+. "$wix_dir\heat.exe" dir $tmp_dir -srd -dr INSTALLDIR -cg MainComponentGroup -out $InstallerPath\directory.wxs -ke -sfrag -gg -var var.SourceDir -sreg -scom
+. "$wix_dir\candle.exe" -dSourceDir="$tmp_dir" -dProductVersion="$version" -dRTMProductVersion="0.0.0" -dUpgradeCode="{1072AB9E-1842-4AFA-9CF2-545462CD60E2}" $InstallerPath\*.wxs -o $InstallerPath\output\ -ext WiXUtilExtension
+. "$wix_dir\light.exe" -o $InstallerPath\output\adldap.msi $InstallerPath\output\*.wixobj -cultures:en-US -ext WixUIExtension.dll -ext WiXUtilExtension -ext WiXNetFxExtension
+. "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\signtool.exe" sign /n "Auth0" $InstallerPath\output\adldap.msi
 
 #Remove the temp
 Remove-Item -Recurse -Force $tmp_dir
