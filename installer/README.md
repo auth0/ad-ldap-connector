daa74a99cfea0fe5ddb1e9d7188b6b95463e0262
Cristofer Gonzales <crigot@hotmail.com>
2016-07-22 15:46:33 -0400

    Adds script to prepare build machine

--- installer/README.md
+++ installer/README.md
@@ -2,12 +2,18 @@ This directory contains the scripts to generate an MSI installer.
 
 ## Pre-requisites
 
-1.  Node for Windows. Check you have node.exe in `C:\Program Files (x86)\nodejs\node.exe`.
-2.  [Wix 3.8](https://wix.codeplex.com/releases/view/115492). Check you have the bin in `c:\Program Files (x86)\WiX Toolset v3.8\bin`
-3.  Microsoft SDK v7.1A. Verify you have `C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\signtool.exe`
-4.  You need to install Auth0's certificate to sign components and installer in your local certificate storage. Just double click on it.
-5.  Your [node-gyp](https://github.com/TooTallNate/node-gyp/#installation) should be able to build native dependencies.
+1.  Visual Studio 2013. Current build pipeline does not work with 2015.
+2.  Node 0.10.x for Windows. Check you have node.exe in `C:\Program Files (x86)\nodejs\node.exe`.
+3.  Update NPM to version 1.4.28
+4.  [Wix 3.8](https://wix.codeplex.com/releases/view/115492). Check you have the bin in `c:\Program Files (x86)\WiX Toolset v3.8\bin`
+5.  Your [node-gyp](https://github.com/TooTallNate/node-gyp/#installation) should be able to build native dependencies. Use version 1.0.2
 6.  Download [nssm](http://nssm.cc/ci/nssm-2.21-136-ga8cb477.zip) and copy the content to `c:\Program Files (x86)\nssm-2.21.1\` folder.
+
+> If you are running on Windows 2012R2 once you have installed VS2013 you can run `prepare.ps1` from an Elevated Powershell console and it will download and install all other components.
+
+If you intent to sign the installer you will also need 
+1.  If you are building on Windows 2008 or earlier versions you need Microsoft SDK v7.1A. Verify you have `C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin\signtool.exe`
+2.  You need to install Auth0's certificate to sign components and installer in your local certificate storage. Just double click on it.
 
 ## How to build
 
---
+++ installer/prepare.ps1
@@ -0,0 +1,168 @@
+$msiDependencies = @(
+    @{'Name'='Python'; 'Url'="https://www.python.org/ftp/python/2.7.12/python-2.7.12.msi";'Path'='C:\Python 2.7\'} ;
+    @{'Name'='nodejs'; 'Url'="https://nodejs.org/dist/v0.10.40/node-v0.10.40-x86.msi"; 'Path' = 'C:\Program Files (x86)\nodejs'}
+    )
+
+$zipDependencies = @(
+    @{'Name'='Wix'; 'Url'="https://wix.codeplex.com/downloads/get/1587180"; 'Path'= "C:\Program Files (x86)\WiX Toolset 3.8\bin"} ;
+    @{'Name'='nssm'; 'Url'="http://nssm.cc/release/nssm-2.24.zip"; 'Path'= "C:\Program Files (x86)\"} 
+    )
+
+$npmPackages = @(
+    @{'Name'='npm'; 'Version'="1.4.28"} ;
+    @{'Name'='node-gyp'; 'Version'="1.0.2"} 
+    )
+
+function Get-TempPath
+{
+    [io.path]::GetTempPath()
+}
+
+function Get-TempFileName
+{
+    [io.path]::GetTempFileName()
+}
+
+function Expand-ZipFile
+{
+    param(
+        [Parameter(Mandatory=$true)][String] $File,
+        [Parameter(Mandatory=$true)][String] $Path
+    )
+
+    if (-not (Test-Path $Path) )
+    {
+        Write-Verbose "Creting Directory $Path"
+        New-Item -Path $Path -ItemType directory -force | Out-Null
+    }
+
+    $shell = New-Object -com Shell.Application
+    $zip = $shell.NameSpace($File)
+    foreach($item in $zip.items())
+    {
+        $shell.Namespace($Path).copyhere($item)
+    }
+}
+
+function InstallFrom-Url
+{
+    param(
+        [Parameter(Mandatory=$true)][String] $Url,
+        [Parameter(Mandatory=$false)][String] $Path= $null,
+        [Parameter(Mandatory=$false)][String] $TempFile = (Get-TempFileName)
+    )
+
+    Write-Verbose "   ==>Downloading installer  $Url ==> $TempFile"
+    Invoke-WebRequest -Uri $Url -OutFile $TempFile
+
+    Write-Verbose "Starting msiexec process"
+    $args = @("/i";$TempFile;"/qb")
+    if ($Path -ne $null) 
+    {
+        $args = @("/i";$TempFile;"TARGETDIR=`"$Path`""; "INSTALLDIR=`"$Path`"", "/qb")
+    }
+    else
+    {
+        $args = @("/i";$TempFile;"/qb")
+    }
+
+    Start-Process -Wait -Verb runas -FilePath msiexec  -ArgumentList $args
+
+    Write-Debug "Removing $TempFile"
+    Remove-Item $TempFile
+}
+
+function UnpackFrom-Url
+{
+    param(
+        [Parameter(Mandatory=$true)][String] $Url,
+        [Parameter(Mandatory=$true)][String] $Path,
+        [Parameter(Mandatory=$false)][String] $TempFile = (Get-TempFileName)
+    )
+
+    $TempFile = $TempFile + ".zip"
+
+    Write-Verbose "   ==> Downloading Package  $Url ==> $TempFile"
+    Invoke-WebRequest -Uri $Url -OutFile $TempFile
+
+    Write-Verbose "Unpacking file into $Path"
+    Expand-ZipFile -File $TempFile -Path $Path
+
+    Write-Debug "Removing $TempFile"
+    #Remove-Item $TempFile
+}
+
+function Install-NpmPackage
+{
+    param(
+        [Parameter(Mandatory=$true)][String] $Name,
+        [Parameter(Mandatory=$false)][String] $Version=$null,
+        [Parameter(Mandatory=$false)][switch] $Global
+    )
+
+    if ($Version -ne $null)
+    {
+        $Name = $Name + "@" + $Version
+    }
+
+    Write-Verbose "Starting NPM install of package $Name"
+
+    if ($global)
+    {
+        $args = @("install";$Name; "-g")
+    }
+    else
+    {
+        $args = @("install";$Name)
+    }
+
+    Start-Process -FilePath "npm" -ArgumentList $args -Wait -NoNewWindow
+}
+
+function Test-IsAdmin() 
+{
+    $windowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $windowsPrincipal = new-object System.Security.Principal.WindowsPrincipal($windowsID)
+    $adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+ 
+    $windowsPrincipal.IsInRole($adminRole)
+}
+
+if (-not (Test-IsAdmin))
+{
+    $args = @('-ExecutionPolicy Bypass';'-File';$PSScriptRoot)
+
+    try
+    {    
+        $process = Start-Process PowerShell.exe -PassThru -Verb Runas -Wait -WorkingDirectory $pwd -ArgumentList $argList
+        exit $process.ExitCode
+    }
+    catch {}
+
+    # Generic failure code
+    exit 1 
+}
+else
+{
+    Write-Host "Installing Dependencies"
+
+    $msiDependencies | ForEach-Object { 
+            Write-Host "==> Installing $($_.Name)" ; 
+            InstallFrom-Url -Url $_.Url -Path $_.Path
+        }
+
+    $zipDependencies | ForEach-Object { 
+            Write-Host "==> Installing $($_.Name)" ; 
+            UnpackFrom-Url -Url $_.Url -Path $_.Path
+         }
+    
+    #reloads path to execute NPM
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+    Write-Host "Updating NPM packages"
+
+    $npmPackages | ForEach-Object { 
+            Write-Host "==> Installing $($_.Name) version $($_.Version)" ; 
+            Install-NpmPackage -Name $_.Name -Version $_.Version -Global
+        }
+}
