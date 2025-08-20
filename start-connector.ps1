c22bc57c60e879e6b04b0adeef3d1c7e7445b52b
Aditya Gaddam <aditya.gaddam@okta.com>
2025-08-19 20:06:30 -0600

    fix: remove need for passing in header size as CLI flag. passing in explicitly into http.createServer

--- start-connector.ps1
+++
@@ -1,3 +0,0 @@
-$json = Get-Content './config.json' | Out-String | ConvertFrom-Json
-$maxHeaderSize = if ($json.MAX_HEADER_SIZE -and $json.MAX_HEADER_SIZE -ne "") { $json.MAX_HEADER_SIZE } else { 16384 }
-node --max-http-header-size=$maxHeaderSize ./server.js 2>&1 | Out-File -FilePath "logs.log" -Encoding ASCII -Append
